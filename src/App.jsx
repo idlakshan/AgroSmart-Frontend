9c589005bc735fd3a8f738e8ff670862ba99a7e7
idlakshan <id.lakshan21@gmail.com>
2025-07-21 10:55:10 +0530

    feat: add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Hero } from './components/Hero'
 import { Navbar } from './components/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
